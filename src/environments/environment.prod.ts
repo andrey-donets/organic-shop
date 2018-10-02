30c00ad26e7685a2d262f023db5936e6e46bb04b
andrey-donets <donets.net@gmail.com>
2018-10-02 19:06:00 +1000

    .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyvMWtypATTY5pRtwol3rvMsnMOtClBZc",
+    authDomain: "oshop-2588d.firebaseapp.com",
+    databaseURL: "https://oshop-2588d.firebaseio.com",
+    projectId: "oshop-2588d",
+    storageBucket: "oshop-2588d.appspot.com",
+    messagingSenderId: "715219559920"
+  }
+};
