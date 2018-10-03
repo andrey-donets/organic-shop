98fb919bf116501b545bc9c9c1fa7213f9f2413e
andrey-donets <donets.net@gmail.com>
2018-10-03 23:57:18 +1000

    Store users in database. Protect the admin routes. Show or hide the admin links.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
-import { Observable } from 'rxjs/Observable';
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import * as firebase from 'firebase';
+
 
 @Injectable()
 export class AuthService {
 user$: Observable <firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
@@ -21,5 +28,10 @@ user$: Observable <firebase.User>;
 
   logout(){
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+    .switchMap(user => this.userService.get(user.uid))
   }
 }
