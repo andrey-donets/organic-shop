778b4ee86c89e7896a83019c1e8edb1019509da0
andrey-donets <donets.net@gmail.com>
2018-10-06 15:35:09 +1000

    Implement searching.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,6 @@
+import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from '../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 
 @Component({
@@ -7,13 +8,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
-  
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: { title: string } [];
+  filteredProducts: any[];
+  subscription: Subscription;
+
   constructor(private productService: ProductService) { 
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+    .subscribe(products => this.filteredProducts = this.products = products);
   }
 
+  filter(query: string){
+    this.filteredProducts = (query) ? 
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
   }
