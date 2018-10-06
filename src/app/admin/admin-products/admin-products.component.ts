4526cf6c604811a648108a55a490b1c268891832
andrey-donets <donets.net@gmail.com>
2018-10-06 16:48:03 +1000

    Feed the DataTable.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,6 +2,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from '../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../../models/product';
+import { DataTableResource } from 'angular-4-data-table';
 
 
 @Component({
@@ -13,10 +14,31 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [] ;
+  itemCount: number;
 
   constructor(private productService: ProductService) { 
     this.subscription = this.productService.getAll()
-    .subscribe(products => this.filteredProducts = this.products = products);
+    .subscribe(products => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    }); 
+  }
+
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+      this.tableResource.query({ offset: 0 })
+        .then(items => this.items = items);
+      this.tableResource.count()
+        .then(count => this.itemCount = count);
+  }
+
+  reloadItems(params){
+    if(!this.tableResource) return;
+
+    this.tableResource.query( params )
+      .then(items => this.items = items);
   }
 
   filter(query: string){
