e1bdd1654d4d865212f4fee66836a8c66c52a89a
andrey-donets <donets.net@gmail.com>
2018-10-07 21:46:49 +1000

    Create a shopping cart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
+import { ShoppingCartService } from './shopping-cart.service';
+import { ProductService } from './product.service';
+import { CategoryService } from './category.service';
+import { AdminAuthGuard } from './admin-auth-guard.service';
+import { UserService } from './user.service';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { RouterModule } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2'; 
+import { AngularFireDatabaseModule } from 'angularfire2/database'; 
+import { AngularFireAuthModule } from 'angularfire2/auth'; 
+import { RouterModule } from '@angular/router'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { FormsModule } from '@angular/forms'; 
+import { CustomFormsModule } from 'ng2-validation'; 
 import { DataTableModule } from 'angular-4-data-table';
 
 import { AppComponent } from './app.component';
@@ -20,17 +28,9 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './login/login.component';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth-guard.service';
-import { UserService } from './user.service';
-import { AdminAuthGuard } from './admin-auth-guard.service';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
-import { CategoryService } from './category.service';
-import { ProductService } from './product.service';
-import { CustomFormsModule } from 'ng2-validation';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
-
 
 @NgModule({
   declarations: [
@@ -65,37 +65,39 @@ import { ProductCardComponent } from './product-card/product-card.component';
       { path: 'login', component: LoginComponent },
 
       { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success', component: OrderSuccessComponent,canActivate: [AuthGuard] },
-      { path:  'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
-
+      { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
+      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] },
+     
       { 
-        path: 'admin/products/new',
-        component: ProductFormComponent,
+        path: 'admin/products/new', 
+        component: ProductFormComponent, 
         canActivate: [AuthGuard, AdminAuthGuard] 
       },
       { 
-        path: 'admin/products/:id',
-        component: ProductFormComponent,
+        path: 'admin/products/:id', 
+        component: ProductFormComponent, 
         canActivate: [AuthGuard, AdminAuthGuard] 
       },
       { 
-        path: 'admin/products',
-        component: AdminProductsComponent,
+        path: 'admin/products', 
+        component: AdminProductsComponent, 
         canActivate: [AuthGuard, AdminAuthGuard] 
       },
-      { path: 'admin/orders',
-        component: AdminOrdersComponent,
-        canActivate: [AuthGuard, AdminAuthGuard] }
-    ])
+      { 
+        path: 'admin/orders', 
+        component: AdminOrdersComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      }
+    ])    
   ],
   providers: [
     AuthService,
     AuthGuard,
-    UserService,
     AdminAuthGuard,
+    UserService,
     CategoryService,
-    ProductService
-
+    ProductService,
+    ShoppingCartService
   ],
   bootstrap: [AppComponent]
 })
