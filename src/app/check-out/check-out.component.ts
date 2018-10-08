b945bf29598656c2f142cacf673517f834b9caa5
andrey-donets <donets.net@gmail.com>
2018-10-08 19:56:59 +1000

    Save the order in Firebase. Associate the order with the current user.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping = {};
+  cart: ShoppingCart;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private shoppingCartServise: ShoppingCartService) {
   }
+
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartServise.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart)
+    this.authService.user$.subscribe(user => this.userId = user.uid)
+  }
+
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+
+ placeOrder() {
+   let order = {
+     userId: this.userId,
+     datePlaced: new Date().getTime(),
+     shipping: this.shipping,
+     items: this.cart.items.map(i => {
+       return {
+         product: {
+           title: i.title,
+           imageUrl: i.imageUrl,
+           price: i.price
+         },
+         quantity: i.quantity,
+         totalPrice: i.totalPrice
+       }
+     })
+   };
+
+   this.orderService.storeOrder(order);
+ }
 
 }
