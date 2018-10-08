edde8acbb6ab80caa0ccc3da70471a53e3de615b
andrey-donets <donets.net@gmail.com>
2018-10-08 14:04:45 +1000

    Fix a Design Issue.

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,16 +1,20 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart { 
-  constructor(public items: ShoppingCartItem[]) {}
-  
-  get productIds() {
-      return Object.keys(this.items);
+    items: ShoppingCart [] = []; 
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+      for (let productId in itemsMap)
+        this.items.push(itemsMap[productId]);
   }
   
+  get productIds() {
+      return Object.keys(this.itemsMap);
+  }
+
   get totalItemsCount() {
     let count = 0;
-    for (let productId in this.items) 
-      count += this.items[productId].quantity;
+    for (let productId in this.itemsMap) 
+      count += this.itemsMap[productId].quantity;
     return count;
   }
 }
