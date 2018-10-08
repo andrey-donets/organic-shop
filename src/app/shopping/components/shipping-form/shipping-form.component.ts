c32b46839b0f30c4f0c40f5bfde0f517f4da27d3
andrey-donets <donets.net@gmail.com>
2018-10-09 02:04:32 +1000

    Create the shopping module.

--- src/app/shopping/components/shipping-form/shipping-form.component.ts
+++ src/app/shopping/components/shipping-form/shipping-form.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Router } from '@angular/router';
-import { AuthService } from '../shared/services/auth.service';
-import { OrderService } from '../shared/services/order.service';
-import { Order } from '../shared/models/Order';
-import { ShoppingCart } from '../shared/models/shopping-cart';
+import { AuthService } from '../../../shared/services/auth.service';
+import { OrderService } from '../../../shared/services/order.service';
+import { Order } from '../../../shared/models/Order';
+import { ShoppingCart } from '../../../shared/models/shopping-cart';
 
 @Component({
   selector: 'shipping-form',
