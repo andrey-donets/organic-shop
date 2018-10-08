badfc07e38c1e77d999d0ed3214c9f2eb5b0cd9e
andrey-donets <donets.net@gmail.com>
2018-10-08 20:32:02 +1000

    Redirect the user upon submitting an order.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { OrderService } from '../order.service';
 import { AuthService } from '../auth.service';
 import { Order } from '../models/Order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
@@ -19,25 +20,27 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   userSubscription: Subscription;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private orderService: OrderService,
-    private shoppingCartServise: ShoppingCartService) {
+    private shoppingCartService: ShoppingCartService) {
   }
 
-  async ngOnInit() {
-    let cart$ = await this.shoppingCartServise.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart)
-    this.authService.user$.subscribe(user => this.userId = user.uid)
+  async ngOnInit() { 
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
-  ngOnDestroy() {
+  ngOnDestroy() { 
     this.cartSubscription.unsubscribe();
     this.userSubscription.unsubscribe();
   }
 
- placeOrder() {
-  let order = new Order(this.userId, this.shipping, this.cart);
-  this.orderService.storeOrder(order);
- }
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
+  }    
 
 }
